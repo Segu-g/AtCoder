bd22f484d8cffa97f6fdfda7c7dfb758fc144cd4
Segu_g <segusegment@gmail.com>
2022-08-13 20:56:27 +0900

    arc138

---
+++ contests/arc138/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+    int64_t *a_arr = new int64_t[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> a_arr[i];
+    }
+}
